44ee9ec5c3e512f6868928417c22e2267833b87f
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-04-04 01:14:42 +0300

    upate use-navigate

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import useReview from '../../hooks/useReview';
 import Review from '../Review/Review';
+import './Home.css'
 
 const Home = () => {
   const [reviews, setReviews] = useReview();
 
-
-
+  const total = reviews.slice(0, 3);
+  const navigate = useNavigate()
   return (
     <>
       <section>
@@ -17,7 +19,7 @@ const Home = () => {
               <div>
                 <h1>Microsoft Xbox </h1>
                 <h2>Series XWireless Controller Shock Blue</h2>
-                <p>Experience the modernized design of the Xbox Wireless Controller in Shock Blue, featuring sculpted surfaces and refined geometry for enhanced comfort during gameplay with battery usage up to 40 hours. Stay on target with a hybrid D-pad and textured grip on the triggers, bumpers, and back-case. </p>
+                <p>Experience the modernized design of the Xbox Wireless Controller in Shock Blue, featuring sculpted surfaces and refined geometry for enhanced comfort during Gameplay with battery usage up to 40 hours. Stay on target with a hybrid D-pad and textured grip on the triggers, bumpers, and back-case. </p>
               </div>
             </Col>
             <Col>
@@ -27,16 +29,22 @@ const Home = () => {
         </Container>
       </section>
 
-      <section>
+      <section className='mb-5'>
         <Container>
+          <h1 className='text-center'>Customer Feedback & Reviews({total.length})</h1>
           <Row>
-            <h1 className='text-center'>Customer Feedback & Reviews</h1>
             <Col>
               <Row> {
-                reviews.map(review => <Review review={review}></Review>)
+                total.map(review => <Review
+                  key={review.id}
+                  review={review}
+                ></Review>)
               }
               </Row>
             </Col>
+            <div className='text-center'>
+              <button onClick={() => navigate('/reviews')} className='see-all-btn'>See All Reviews</button>
+            </div>
           </Row>
         </Container>
       </section>
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import useReview from '../../hooks/useReview';
+import Review from '../Review/Review';
 
 const Reviews = () => {
+  const [reviews, setReviews] = useReview();
+
   return (
-    <div>
-      <h1>Customer Review Pages</h1>
-    </div>
+    <section className='mb-5'>
+      <Container>
+        <Row>
+          <Col>
+            <Row> {
+              reviews.map(review => <Review
+                key={review.id}
+                review={review}
+              ></Review>)
+            }
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
 
